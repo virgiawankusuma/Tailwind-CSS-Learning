c0818d3f48de75c2bc8e7b36b6d2f3a4b7acfca9
virgiawankusuma <virgiawan150@gmail.com>
2022-06-30 16:01:19 +0700

    initialization tailwind on CRA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const element = <h1>Coba</h1>;
+import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<App/>);

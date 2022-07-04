500d3d94876f2cbde2b01f118fddb72dd771559f
virgiawankusuma <virgiawan150@gmail.com>
2022-07-04 10:33:01 +0700

    making article list with basic utility tailwind (background behaviour, blend mode

--- src/components/AppHero.js
+++ src/components/AppHero.js
@@ -4,14 +4,13 @@ import AppHeroBody from './AppHeroBody';
 function AppHero() {
   return (
     <div 
-    className="min-h-screen bg-cover bg-bottom bg-no-repeat mb-6 bg-fixed" 
+    className="min-h-screen bg-cover bg-center bg-no-repeat mb-6 bg-fixed bg-gray-400 bg-blend-multiply" 
     style={{
-      backgroundImage: "url('https://source.unsplash.com/1000x600?mountain')"
+      backgroundImage: `url('https://source.unsplash.com/1000x600?mountain')`
       }}>
-        <div className="w-full h-full bg-black mix-blend-multiply"></div>
         <AppHeroBody/>
     </div>
-  );
+  );  
 }
 
 export default AppHero;

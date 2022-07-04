9f5e99b3fc0a997a2ebd77539f82cebcd59e4ce8
virgiawankusuma <virgiawan150@gmail.com>
2022-07-04 13:53:39 +0700

    add first letter pseudo class

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function ArticleItem({ title, excerpt, author, username }) {
   return (
-    <div className="p-4 shadow-yellow-200 shadow-md mb-6 max-w-lg mx-auto bg-white">
-      <h2 className="mb-3 font-playfairDisplay text-3xl">{title}</h2>
-      <div className="font-poppins">
+    <div className="px-6 py-4 rounded-lg border shadow-orange-200 shadow mb-6 max-w-lg mx-auto bg-white hover:shadow-lg hover:shadow-orange-200 hover:bg-orange-500 group">
+      <h2 className="mb-3 font-playfairDisplay text-3xl group-hover:text-white">{title}</h2>
+      <div className="font-poppins group-hover:text-white">
         <div className="mb-2">
-          By <a href={`https://example.com/username/${username}`} className="font-bold text-slate-700 hover:text-slate-400">{author}</a>
+          By <a href={`https://example.com/username/${username}`} className="font-bold text-slate-700 hover:text-slate-400 group-hover:text-slate-300">{author}</a>
         </div>
-        <p>{excerpt}</p>
+        <p className="group-hover:text-white first-letter:text-5xl first-letter:font-playfairDisplay first-letter:float-left first-letter:mr-2 first-letter:-mt-2">{excerpt}</p>
       </div>
     </div>
   );

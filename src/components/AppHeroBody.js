57dc2e9e9c70a3345707adde742064278476633b
virgiawankusuma <virgiawan150@gmail.com>
2022-07-04 11:28:37 +0700

    add pseudo class in CTA button

--- src/components/AppHeroBody.js
+++ src/components/AppHeroBody.js
@@ -8,6 +8,7 @@ function AppHeroBody() {
         <p className="font-poppins text-3xl mt-4 tracking-wide leading-relaxed">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iure, deserunt veritatis, debitis iusto aut autem, atque porro quo fugiat culpa laudantium ex eaque quas? Est iure quasi optio obcaecati?
         </p>
+        <button className="bg-orange-400 rounded-full px-6 py-2 text-2xl mt-5 text-white hover:bg-orange-500 active:bg-orange-600 focus:ring focus:ring-orange-200">Get Started</button>
       </div>
     </div>
   );
